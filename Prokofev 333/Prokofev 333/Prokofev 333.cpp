442f532471a312fc42918ea45bacf26d2c0a5cf8
Chelkameshaet <150122723+Chelkameshaet@users.noreply.github.com>
2023-11-13 11:23:16 +0300

    Add files via upload

---
+++ Prokofev 333/Prokofev 333/Prokofev 333.cpp
@@ -0,0 +1,16 @@
+﻿// Prokofev 333.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    setlocale(0, "");
+    long long factorial = 1;
+    for (int i = 1; i <= 15; ++i) {
+        factorial *= i;
+    }
+    cout << "Факториал числа 15: " << factorial << std::endl;
+
+    return 0;
+}
